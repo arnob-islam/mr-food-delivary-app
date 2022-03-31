b654ca67e8e311ef06cc9cd4592d1aa1e33796b4
arnob-islam <mr.arnob101@gmail.com>
2022-03-31 22:38:29 +0600

    initial commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,38 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import "./style/app.css"
-import "./style/indeed.css"
-import "./style/admin.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./style/app.css";
+import "./style/indeed.css";
+import "./style/admin.css";
 
-import 'antd/dist/antd.css';
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, } from '@apollo/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import Context from './components/Provider/Context'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import { RootReducer } from './Redux/Provider'
+import "antd/dist/antd.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import Context from "./components/Provider/Context";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { RootReducer } from "./Redux/Provider";
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:5000/graphql', credentials: 'include',
-})
-
+  uri: "http://localhost:5000/graphql",
+  credentials: "include",
+});
 
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
-  credentials: 'include',
-})
+  credentials: "include",
+});
 
+// use this to inspact in console
+// const store = createStore(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-const store = createStore(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
+const store = createStore(RootReducer);
 
 ReactDOM.render(
   <React.Fragment>
-    <Provider store={store} >
+    <Provider store={store}>
       <Router>
-        <ApolloProvider client={client} >
+        <ApolloProvider client={client}>
           <Context>
             <App />
           </Context>
@@ -40,6 +46,5 @@ ReactDOM.render(
       </Router>
     </Provider>
   </React.Fragment>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
